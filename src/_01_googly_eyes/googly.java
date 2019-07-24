dd5b774038795722337f320af9f37cd6f1f12c55
ilabstaff <ilabstaff@172.16.116.68>
2019-07-23 18:01:13 -0700

    googly_eyes

---
+++ src/_01_googly_eyes/googly.java
@@ -0,0 +1,57 @@
+
+package _01_googly_eyes;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class googly {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		void setup(){
+			 size(800,600);
+			  PImage face = loadImage("oopo.jpeg");
+			     face.resize(width,height);
+			      background(face);
+			}
+
+			void draw(){
+			  
+			  ellipse(214, 279, 55, 45);
+			  ellipse(303, 263, 55, 45);
+
+			  
+			  
+			  
+
+			}
+
+	}
+	
+	
+	
+	
+  
+	}
+   
